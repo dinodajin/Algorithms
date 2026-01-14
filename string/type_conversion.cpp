facb813889474b7fc007f010c96734c431e4285f
djbrachio <djrbachio@gmail.com>
2026-01-14 23:26:37 +0900

    solve :31403 A + B - C

---
+++ string/type_conversion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    string A, B, C;
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    cout << stoi(A) + stoi(B) - stoi(C) << "\n";
+    cout << stoi(A + B) - stoi(C);
+    return 0;
+}
