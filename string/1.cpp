b411ccd86946c7306f12330c92508607abcadc49
djbrachio <djrbachio@gmail.com>
2026-01-14 23:02:18 +0900

    solve : 27866 문자와 문자열

---
+++ string/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+    string S;
+    int num;
+
+    cin >> S;
+    cin >> num;
+
+    cout << S[num - 1];
+    return 0;
+}
